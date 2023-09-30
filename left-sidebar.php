e34d7949483e68b1c31ffb192da0c584d3061445
Md. Nazmul Siddique <27219@wgbd.com>
2023-09-30 11:12:17 +0600

    some correction done

--- left-sidebar.php
+++ left-sidebar.php
@@ -14,9 +14,9 @@
     </header>
     <div class="info">
 			<h2>Contact US</h2>
-			<p>Mail:</p>
-			<p>PHone:</p>
-			<p>Website:</p>
+			<p>Mail: global@waltonplc.com</p>
+			<p>Phone: 008809606-555555</p>
+			<p>Address:1088, Block-I, Sabrina Sobhan Road, P.O-Khilkhet, P.S-Vatara, Bashundhara R/A, Dhaka-1229</p>
 
 		</div>
     </div>
