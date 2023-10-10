5427dd8fbb85447aae77e218e2d8be3edac52ec7
Md. Nazmul Siddique <27219@wgbd.com>
2023-10-10 11:56:01 +0600

    catalogues.php file added

---
+++ catalogues.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  	<link rel="stylesheet" href="css/bootstrap.min.css">
+  	<link rel="stylesheet" href="css/style.css">
+	<title>Catalogues</title>
+</head>
+<body>
+
+	<img src="https://waltonbd.com/image/catalog/category-banner/refrigerator/freezer-to-fridge-switch-whenever-you-need.jpg" alt="" class="img-fluid">
+	<section id="home-info">
+		<div class="container">
+			<div class="row">
+				<div class="col-4">			
+
+				</div>			
+				<div class="col-4 text-center">
+				<a href="https://waltonbd.com">			
+					<img src="pics/walton.png" class="img-fluid" alt="">
+					<p>Visit our website</p>
+				</a>	
+				</div>
+				<div class="col-4">			
+
+				</div>				
+			</div>
+		</div>
+
+		<div class="container">
+		<h2 class="title-heading mb-5">All Catalogues</h2>
+			<div class="row text-center">
+
+				<div class="col-md-4 mb-5">
+					<div class="catalogue-box">
+					<h3>Refrigerator</h3>
+					<img src="pics/ref.jpg" class="img-fluid rounded py-3" alt="">					
+					<button class="btn btn-outline-dark">Download Catalogue</button>
+					</div>
+				</div>
+				<div class="col-md-4 mb-5">
+					<div class="catalogue-box">				
+					<h3>Air Conditioner</h3>
+					<img src="pics/ac.jpg" class="img-fluid rounded py-3" alt="">					
+					<button class="btn btn-outline-dark">Download Catalogue</button>
+					</div>
+				</div>
+
+				<div class="col-md-4 mb-5">
+					<div class="catalogue-box">					
+					<h3>Fan</h3>
+					<img src="pics/tv.jpg" class="img-fluid rounded py-3" alt="">					
+					<button class="btn btn-outline-dark">Download Catalogue</button>
+					</div>					
+				</div>				
+				
+			</div>
+			<div class="row text-center justify-content-center">			
+				<div class="col-md-4 mb-5">
+					<div class="catalogue-box">				
+					<h3>ACC Brand</h3>
+					<img src="pics/wm.jpg" class="img-fluid rounded py-3" alt="">					
+					<button class="btn btn-outline-dark">Download Catalogue</button>
+					</div>					
+				</div>
+				<div class="col-md-4 mb-5">
+					<div class="catalogue-box">					
+					<h3>All Catalogues</h3>
+					<img src="pics/wm.jpg" class="img-fluid rounded py-3" alt="">					
+					<button class="btn btn-outline-dark">Download Catalogue</button>
+					</div>					
+				</div>				
+			</div>
+		</div>
+	</section>
+	<!--Newsletter-->
+
+	<section id="newsletter" class="py-5 text-center bg-dark text-light">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">			
+					<h2 class="text-light">Contact Us</h2>
+					<p class="lead">Global Business :<br>
+                        Email: global@waltonplc.com<br>
+                        Telephone: 008809606-555555<br>
+                        Plot-1088, Block-I, Sabrina Sobhan Road<br>
+                        P.O-Khilkhet, P.S-Vatara,<br>
+                        Bashundhara R/A, Dhaka-1229.
+                    </p>
+
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!--Footer Section-->
+
+	<script src="js/bootstrap.min.js"></script>
+</html>
